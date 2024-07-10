61a2896615182abf599b8ffa2be6c30d86975ef3
akinpinkman <akinbilgin54@hotmail.com>
2024-07-10 15:14:48 +0300

    Add cart module and component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,12 +8,14 @@ import { ProductModule } from './product/product.module';
 import { HttpClientModule } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { CartModule } from './cart/cart.module';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     importProvidersFrom(ProductModule),
+    importProvidersFrom(CartModule),
     importProvidersFrom(HttpClientModule),
     provideAnimationsAsync(),
   ],
---
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Product } from '../../models/product';
+
+@Component({
+  selector: 'app-cart-view',
+  templateUrl: './cart-view.component.html',
+  styleUrl: './cart-view.component.css',
+})
+export class CartViewComponent implements OnInit {
+  cardItems: Product[] = [];
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartService.getCartItems().subscribe((data) => {
+      this.cardItems = data;
+    });
+  }
+}
